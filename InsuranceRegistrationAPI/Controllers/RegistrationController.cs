40a7036e7aefaf8cbba119b0701255920adedb0c
serkancakir <36895589+serkancakir@users.noreply.github.com>
2021-02-26 23:48:40 +0000

    Added Service and Repository to startup

--- InsuranceRegistrationAPI/Controllers/RegistrationController.cs
+++ InsuranceRegistrationAPI/Controllers/RegistrationController.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsuranceRegistrationAPI.Models;
+using InsuranceRegistrationAPI.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InsuranceRegistrationAPI.Controllers
 {
     [Route("api")]
     public class RegistrationController : Controller
     {
-        public RegistrationController()
+        private readonly ICustomerService _customerService;
+        public RegistrationController(ICustomerService customerService)
         {
+            _customerService = customerService;
         }
 
         [HttpGet]
         public string Get()
         {
             return "Running...";
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register([FromBody] Customer customerInformation)
+        {
+            var result = _customerService.CreateCustomer(customerInformation);
+
+            return Ok(result.CustomerId);
         }
     }
 }
--- InsuranceRegistrationAPI/Startup.cs
+++ InsuranceRegistrationAPI/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using InsuranceRegistrationAPI.Repositories;
+using InsuranceRegistrationAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace InsuranceRegistrationAPI
 {
@@ -26,6 +21,8 @@ namespace InsuranceRegistrationAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ICustomerService, CustomerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
