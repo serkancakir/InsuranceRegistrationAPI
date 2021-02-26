c412f8e2761360cd6d90b7c6b082af49d51091fc
serkancakir <36895589+serkancakir@users.noreply.github.com>
2021-02-26 23:22:21 +0000

    Project Initialization

---
+++ InsuranceRegistrationAPI/Controllers/RegistrationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceRegistrationAPI.Controllers
+{
+    [Route("api")]
+    public class RegistrationController : Controller
+    {
+        public RegistrationController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Running...";
+        }
+    }
+}
