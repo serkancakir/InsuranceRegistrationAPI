c31d6364b3c5ac8982a25373aa0f03a64d16ac38
serkancakir <36895589+serkancakir@users.noreply.github.com>
2021-02-27 01:11:24 +0000

    Added unit test

---
+++ InsuranceRegistrationAPITest/RegistrationControllerTest.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using InsuranceRegistrationAPI.Controllers;
+using InsuranceRegistrationAPI.DTO;
+using InsuranceRegistrationAPI.Mappings;
+using InsuranceRegistrationAPI.Models;
+using InsuranceRegistrationAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace InsuranceRegistrationAPITest
+{
+    [TestClass]
+    public class RegistrationControllerTest
+    {
+        private Mock<ICustomerService> mockService;
+        private MapperConfiguration mockMapper;
+        private IMapper mapper;
+        private RegistrationController controller;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            mockService = new Mock<ICustomerService>();
+
+            mockService.Setup(x => x.CreateCustomer(It.IsAny<Customer>()))
+                .Returns(new Customer { CustomerId = 1 });
+
+            mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ResourceToModelProfile());
+            });
+            mapper = mockMapper.CreateMapper();
+
+            controller = new RegistrationController(mockService.Object, mapper);
+
+        }
+        [TestMethod]
+        public void GetMethodShouldReturnSuccess()
+        {
+            var result = controller.Get();
+            Assert.AreEqual("Running...", result);
+        }
+
+        [TestMethod]
+        public void RegisterTest()
+        {
+            CustomerDTO customerInfo = new CustomerDTO
+            {
+                CustomerName = "Serkan",
+                CustomerSurname = "Cakir",
+                PolicyReferenceNumber = "SC-123456",
+                Email = "test_-.120sd@asd_-.123.co.uk"
+
+            };
+            var result = controller.Register(customerInfo);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, ((OkObjectResult)result).Value);
+            Assert.AreEqual(Microsoft.AspNetCore.Http.StatusCodes.Status200OK, ((OkObjectResult)result).StatusCode);
+        }
+    }
+}
